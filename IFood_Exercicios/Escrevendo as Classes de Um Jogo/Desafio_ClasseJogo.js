353c4222ff9f66f5da626e22f71fa8cf923d23d6
Rozvania27 <145517065+Rozvania27@users.noreply.github.com>
2023-12-17 17:07:44 -0300

    Desafio_ClassJogo

--- IFood_Exercicios/Escrevendo as Classes de Um Jogo/Desafio_ClasseJogo.js
+++ IFood_Exercicios/Escrevendo as Classes de Um Jogo/Desafio_ClasseJogo.js
@@ -1,39 +1,39 @@
 class Heroi {
     constructor(nome, idade, tipo) {
-      this.nome = nome;
-      this.idade = idade;
-      this.tipo = tipo;
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
     }
 
-    atacar() { 
-    
-     let ataque="";
+    atacar() {
 
-     if (this.tipo == "guerreiro") {
-         ataque = 'espada';
+        let ataque = "";
 
-     } else if (this.tipo == "mago") {
-         ataque = "magia"
-          
-     } else if (this.tipo == "monge") {
-        ataque = 'artes marciais';
-          
-      } else{
-          ataque = 'shuriken';
-     }
-  
-      console.log (`O ${this.tipo} ${this.nome}, ${this.idade} anos, atacou usando ${ataque}`)
+        if (this.tipo == "guerreiro") {
+            ataque = 'espada';
+
+        } else if (this.tipo == "mago") {
+            ataque = "magia"
+
+        } else if (this.tipo == "monge") {
+            ataque = 'artes marciais';
+
+        } else {
+            ataque = 'shuriken';
+        }
+
+        console.log(`O ${this.tipo} ${this.nome}, ${this.idade} anos, atacou usando ${ataque}`)
     }
-    }  
- 
-  const heroi1 = new Heroi('Rubio Hagrid', 45 , 'mago');
-  const heroi2 = new Heroi('He-Man', 22, 'guerreiro');
-  const heroi3 = new Heroi('Obadiah', 17, 'monge');
-  const heroi4 = new Heroi('Jiraiya',  3, 'ninja');
-  
-  
-  heroi1.atacar();
-  heroi2.atacar();
-  heroi3.atacar();
-  heroi4.atacar();
-    
+
+}
+
+const heroi1 = new Heroi('Rubio Hagrid', 45, 'mago');
+const heroi2 = new Heroi('He-Man', 22, 'guerreiro');
+const heroi3 = new Heroi('Obadiah', 17, 'monge');
+const heroi4 = new Heroi('Jiraiya', 3, 'ninja');
+
+
+heroi1.atacar();
+heroi2.atacar();
+heroi3.atacar();
+heroi4.atacar();
